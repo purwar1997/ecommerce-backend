26eab2cb499a25116146691d92ebd4a64ac9c474
purwar1997 <shubhampurwar511@gmail.com>
2024-06-05 02:24:16 +0530

    feat: add route that allow admins to fetch user by ID

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,11 +1,29 @@
+import User from '../models/User.js';
 import handleAsync from '../services/handleAsync.js';
+import CustomError from '../utils/customError.js';
 
 export const getUserDetails = handleAsync(async (_req, res) => {
   const { user } = res;
 
   res.status(200).json({
     success: true,
-    message: 'User details successfully fetched',
+    message: 'User details fetched successfully',
+    user: user.toObject({ virtuals: true }),
+  });
+});
+
+export const getUserById = handleAsync(async (req, res) => {
+  const { userId } = req.params;
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw new CustomError('User not found', 404);
+  }
+
+  res.status(200).json({
+    success: true,
+    message: 'User fetched by ID successfully',
     user: user.toObject({ virtuals: true }),
   });
 });
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getUserDetails } from '../controllers/userControllers.js';
+import { getUserDetails, getUserById } from '../controllers/userControllers.js';
 import { authenticate } from '../middlewares/authenticate.js';
 
 const router = express.Router();
 
 router.route('/user').get(authenticate, getUserDetails);
+router.route('/user/:userId').get(getUserById);
 
 export default router;
