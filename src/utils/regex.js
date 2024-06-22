5878bac7fcd497b5eef279578ba4d4e41cbd597d
purwar1997 <shubhampurwar511@gmail.com>
2024-06-22 19:32:07 +0530

    Update regular expression for postal code

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -3,5 +3,5 @@ export const fullnameRegex = /^[A-Za-z\s]*$/;
 export const emailRegex = /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 export const phoneRegex = /(0|91)?[6-9][0-9]{9}/;
 export const passwordRegex = /^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,20}$/;
-export const postalCodeRegex = /^[1-9]{1}[0-9]{2}s{0,1}[0-9]{3}$/;
+export const postalCodeRegex = /^[0-9A-Z]{2,4}[ -]?[0-9A-Z]{3,4}$/;
 export const imageUrlRegex = /^https?:\/\/.+\.(jpg|jpeg|png|gif)$/;
