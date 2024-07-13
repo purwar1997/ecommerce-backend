3b7713f4613546a062d7d084cdda635ca109d8de
purwar1997 <shubhampurwar511@gmail.com>
2024-07-13 12:40:28 +0530

    Replace folder option with asset_folder in cloudinary upload API

--- src/services/cloudinaryAPIs.js
+++ src/services/cloudinaryAPIs.js
@@ -2,15 +2,15 @@ import cloudinary from '../config/cloudinary.config.js';
 import CustomError from '../utils/customError.js';
 import { parseDate } from '../utils/helpers.js';
 
-const generatePublicId = docId => {
+const generatePublicId = (folder, docId) => {
   const timestamp = parseDate(new Date());
-  return `${timestamp}/${docId}`;
+  return `${folder}/${timestamp}/${docId}`;
 };
 
 export const uploadImage = async (folder, file, docId) => {
   const options = {
-    public_id: generatePublicId(docId),
-    folder,
+    public_id: generatePublicId(folder, docId),
+    asset_folder: folder,
   };
 
   try {
