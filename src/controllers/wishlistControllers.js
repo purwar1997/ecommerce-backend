03202c24657556c1d588acb242da0afb78a1b76e
purwar1997 <shubhampurwar511@gmail.com>
2024-07-18 21:52:56 +0530

    feat(api): add endpoint that allows users to add items to their wishlist

--- src/controllers/wishlistControllers.js
+++ src/controllers/wishlistControllers.js
@@ -14,3 +14,25 @@ export const getWishlist = handleAsync(async (req, res) => {
 
   sendResponse(res, 200, 'Wishlist fetched successfully', user.wishlist);
 });
+
+export const addItemToWishlist = handleAsync(async (req, res) => {
+  const { productId } = req.body;
+  const { user } = req;
+
+  const product = await Product.findOne({ _id: productId, isDeleted: false });
+
+  if (!product) {
+    throw new CustomError('Product not found', 404);
+  }
+
+  const productExistsInWishlist = user.wishlist.includes(productId);
+
+  if (productExistsInWishlist) {
+    throw new CustomError('Item already exists in wishlist', 409);
+  }
+
+  user.wishlist.push(productId);
+  await user.save();
+
+  sendResponse(res, 200, 'Item added to wishlist successfully', product);
+});
--- src/routes/wishlistRoutes.js
+++ src/routes/wishlistRoutes.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import { getWishlist } from '../controllers/wishlistControllers.js';
+import { getWishlist, addItemToWishlist } from '../controllers/wishlistControllers.js';
 import { isAuthenticated } from '../middlewares/authMiddlewares.js';
+import { validatePayload } from '../middlewares/requestValidators.js';
+import { productIdSchema } from '../schemas/wishlistSchemas.js';
 
 const router = express.Router();
 
 router.route('/wishlist').get(isAuthenticated, getWishlist);
+
+router
+  .route('/wishlist/add')
+  .put(isAuthenticated, validatePayload(productIdSchema), addItemToWishlist);
 
 export default router;
