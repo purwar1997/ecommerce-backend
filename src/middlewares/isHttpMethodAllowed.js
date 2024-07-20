d49b90c7c24c1a714beb7e8efedd6899ec8c127a
purwar1997 <shubhampurwar511@gmail.com>
2024-07-20 08:45:45 +0530

    Add req.baseUrl in error message

--- src/middlewares/isHttpMethodAllowed.js
+++ src/middlewares/isHttpMethodAllowed.js
@@ -6,12 +6,11 @@ export const isHttpMethodAllowed = handleAsync((req, res, next) => {
   const allowedMethods = expectedHttpActions[req.path];
 
   if (!allowedMethods || !allowedMethods.length) {
-    throw new CustomError(`No allowed methods defined for path: ${req.path}`, 500);
+    throw new CustomError(`No allowed methods defined for path: ${req.baseUrl + req.path}`, 500);
   }
 
   if (!allowedMethods.includes(req.method)) {
     res.set('Allow', allowedMethods.join(', '));
-
     throw new CustomError(`${req.method} method not allowed on this route`, 405);
   }
 
