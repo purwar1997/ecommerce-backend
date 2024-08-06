b5dce432ae9bc7be6815fe12a3b5bac73e626725
purwar1997 <shubhampurwar511@gmail.com>
2024-08-06 22:50:55 +0530

    Check if cron job is running on a port or not

--- src/cron/scheduler.js
+++ src/cron/scheduler.js
@@ -1,6 +1,23 @@
+import express from 'express';
+import cors from 'cors';
 import cron from 'node-cron';
 import { checkCouponExpiry } from './index.js';
 import { CRON_JOB } from '../constants/common.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  console.log('Home route');
+
+  res.status(200).json({
+    message: 'Home route',
+  });
+});
+
+app.listen(process.env.CRON_PORT, () => console.log(`Server is running on port 5000`));
 
 const scheduleJob = (cronExpression, jobFunction) => {
   cron.schedule(cronExpression, jobFunction, {
@@ -10,3 +27,4 @@ const scheduleJob = (cronExpression, jobFunction) => {
 };
 
 scheduleJob('0 0 * * *', checkCouponExpiry);
+scheduleJob('*/5 * * * * *', () => console.log('Task running every 5 seconds'));
